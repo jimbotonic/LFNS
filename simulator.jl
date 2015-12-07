903534c54e8de1f37778af34e54983d636fab3cb
jimbotonic <jimmy.dubuisson@gmail.com>
2015-12-07 17:20:31 +0100

    new types

--- simulator.jl
+++ simulator.jl
@@ -1,24 +1,36 @@
-# initial conditions
-type ICConditions
-	P::Array{Float64,1}
+include("graphs.jl")
+
+abstract  Change
+
+# vertex change
+type VChange <: Change
+	# index -> new value
+	nvs::Dict{Int,Float64}	
+end
+
+# edge change
+type EChange <: Change
+	i::Int
+	j::Int
+	nv::Float64
+end 
+
+# state of the system at a given index
+type State
+	i::Int
 	V::Array{Float64,1}
 	T::Array{Float64,1}
-	Y::Array{Complex{Float64},2}
-end
-
-type Changes
-	
-end
-
-type Store
-	T
+	Tdot::Array{Float64,1}
 end
 
 type Simulator
-	# initial conditions
-	IC::IConditions
+	# initial conditions (specifiy P,V,T,Y) 
+	g::Graphs.AbstractGraph{Bus,Line}
+	# convergence criteria
+	epsilon::Float64
+	iter_max::Int64	
 	# list of changes
-	
-	# store
-	s::Store	
+	changes::Array{Change,1}	
+	# states history
+	states::Array{State,1}
 end
