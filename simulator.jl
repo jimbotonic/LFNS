77c263a5ece542e3fa01624dd21f23452f08a0bb
jimbotonic <jimmy.dubuisson@gmail.com>
2015-12-07 14:17:50 +0100

    new types

--- simulator.jl
+++ simulator.jl
@@ -1,3 +1,24 @@
+# initial conditions
+type ICConditions
+	P::Array{Float64,1}
+	V::Array{Float64,1}
+	T::Array{Float64,1}
+	Y::Array{Complex{Float64},2}
+end
+
+type Changes
+	
+end
+
+type Store
+	T
+end
+
 type Simulator
 	# initial conditions
+	IC::IConditions
+	# list of changes
+	
+	# store
+	s::Store	
 end
