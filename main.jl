c1eaaa143426a033529faefb72061c4befc6fd6a
jimbotonic <jimmy.dubuisson@gmail.com>
2016-02-29 13:10:43 +0100

    parallelization

--- main.jl
+++ main.jl
@@ -1,9 +1,9 @@
 using ArgParse, Logging
 
- include("init.jl")
- include("data.jl")
- include("solvers.jl")
- include("graphs.jl")
+include("init.jl")
+include("data.jl")
+include("solvers.jl")
+include("graphs.jl")
 
 # @Logging.configure(level=INFO)
 
@@ -34,23 +34,23 @@ function parse_cl()
 			help = "file name of the boostrap T vector"
 			required = false
 		"--iter"
-			help = "iteration number, if parallel=yes : iteration number within the subinterval of alpha"
+			help = "iteration number (if parallel=yes : iteration number within the subinterval of alpha"
 			required = false
 		"--g_fn"
 			help = "file name for serialized graph"
 			required = false
 		"--parallel"
-			default = "no"
-			help = "yes if parallel computing"
+			default = "1"
+			help = "1: no parallelization | 2: subinterval | 3: parallelization"
+			required = false
+		"--nprocs"
+			help = "# of processes to allocate for parallelization"
 			required = false
 		"--alpha_i"
-			help = "first alpha value when parallelizing"
-			required = false
-		"--step_num"
-			help = "number of steps for each instance when parallelizing"
+			help = "first alpha value (parallel=2)"
 			required = false
 		"--alpha_interval_length"
-			help = "length of alpha interval when parallelizing"
+			help = "length of alpha interval (parallel=2)"
 			required = false
 	end
 	return parse_args(s)
@@ -120,11 +120,11 @@ elseif solver == "SD"
 
 	export_csv_data(state.T, "T_out.csv")
 elseif solver == "KR"
-	@everywhere function get_state(s::Simulator,P_ref::Array{Float64,1},alpha::Float64,max_value::Float64)
-		P = P_ref*alpha*max_value
+	function get_state(s::Simulator,P_ref::Array{Float64,1},t::Float64)
+		P = P_ref*t
 		@debug("norm P (2/Inf): ", norm(P,2), "/", norm(P,Inf))
 		change_P(s.g,P)
-		return t,simulation(s)
+		return simulation(s)
 	end
 	
 	g = load_serialized(pargs["g_fn"])
@@ -134,10 +134,10 @@ elseif solver == "KR"
 	sb = 1.
 	max_iter = round(Int64,1e6)
 	
-	epsilon = 1e-3
+	epsilon = 1e-6
 	# NB: Eurogrid PC has 6021 nodes and a max degree of 17
 	# critical value is between 800 and 850
-	max_value = 600.
+	max_value = 800.
 
 	ssolver = pargs["ssolver"]
 	if ssolver == "SD"
@@ -173,7 +173,7 @@ elseif solver == "KR"
 	end
 
 	# array of computed states 
-	states = Dict{Int,Array{Float64,1}}()
+	states = Dict{Float64,Array{Float64,1}}()
 
 	# parallel mode	
 	par = parse(Int,pargs["parallel"])
@@ -183,41 +183,61 @@ elseif solver == "KR"
 		tic()
 		for j in 1:iter
 			# start at 0 (ie, "flat start")
-			t = (j-1)/iter	
+			alpha = (j-1)/iter	
 			if j > 1
 				change_T(s.g,state.T)
 			end	
-			@info("simulation # $j (alpha=$t max=$max_value)")
-			i,state = get_state(s,P_ref,t,max_value)
+			@info("simulation # $j (alpha=$alpha max=$max_value)")
+			t = alpha*max_value
+			state = get_state(s,P_ref,t)
 			@info("----------")
 			
-			states[i] = state.T
+			states[t] = state.T
 		end
 		toc()
 		serialize_to_file(states, "states_$u_name-$iter-$max_value.jld")
 	# simulation on a subinterval of alpha : [alpha_i , alpha_i + alpha_interval_length]	
 	elseif par == 2
 		tic()
-		# step_num = parse(Int,pargs["step_num"])
 		alpha_i = parse(Float64,pargs["alpha_i"])
 		alpha_interval_length = parse(Float64,pargs["alpha_interval_length"])
 		for j in 1:iter
-			t = alpha_i + (j-1)*alpha_interval_length/iter
+			alpha = alpha_i + (j-1)*alpha_interval_length/iter
 			if j > 1
 				change_T(s.g,state.T)
 			end
-			@info("simulation # $j (alpha=$t max=$max_value)")
-			i,state = get_state(s,P_ref,t,max_value)
+			@info("simulation # $j (alpha=$alpha max=$max_value)")
+			t = alpha*max_value
+			state = get_state(s,P_ref,t)
 			@info("----------")
 			
-			states[i] = state.T
+			states[t] = state.T
 		end
 		toc()
 		serialize_to_file(states, "states_$u_name-$max_value-$alpha_i.jld")
 	elseif par == 3
-		addprocs(4)
+		# number of processes to be used
+		nprocs = parse(Int,pargs["nprocs"])
+		addprocs(nprocs)
+
+		# include the required code
+		@everywhere include("init.jl")
+		@everywhere include("data.jl")
+		@everywhere include("solvers.jl")
+		@everywhere include("graphs.jl")
+		@everywhere function get_state(s::Simulator,P_ref::Array{Float64,1},t::Float64)
+			P = P_ref*t
+			@debug("norm P (2/Inf): ", norm(P,2), "/", norm(P,Inf))
+			change_P(s.g,P)
+			return t,simulation(s)
+		end
 		tic()
-		results = pmap(get_state,Simulator[s for j in 1:iter],Array{Float64,1}[P_ref for j in 1:iter],Float64[(j-1)/iter for j in 1:iter],Float64[max_value for j in 1:iter])	
+		@sync results = pmap(get_state,Simulator[s for j in 1:iter],Array{Float64,1}[P_ref for j in 1:iter],Float64[((j-1)/iter)*max_value for j in 1:iter])	
 		toc()
+		
+		for r in results
+			states[r[1]] = r[2].T 
+		end		
+		serialize_to_file(states, "states_$u_name-$iter-$max_value.jld")
 	end
 end
