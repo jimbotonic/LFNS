2bf371773e5e28a1a9e06436f2d6e0d7cc3e277f
jimbotonic <jimmy.dubuisson@gmail.com>
2016-05-22 23:51:44 +0200

    vortex analysis

--- utils/analyze_vortex_stability2.jl
+++ utils/analyze_vortex_stability2.jl
@@ -13,7 +13,7 @@ include("../metrics.jl")
 conf = ConfParse("../config.ini")
 parse_conf!(conf)
 
-addprocs(45)
+addprocs(3)
 
 @everywhere begin
 	include("../init.jl")
@@ -43,28 +43,43 @@ s = Simulator(g,RK_solver1,o_args,sb,epsilon,max_iter)
 # get the contour cycle of the lattice
 bcycle =  get_sq_lattice_contour_cycle(n,m)
 
-# central face cycle
-@everywhere begin 
-	ccycle = Int[]
-	ipos = ceil(Int,m/2)
-	jpos = ceil(Int,n/2)
 
-	push!(ccycle, (ipos-1)*n+jpos)
-	push!(ccycle, (ipos-1)*n+jpos+1)
-	push!(ccycle, ipos*n+jpos+1)
-	push!(ccycle, ipos*n+jpos)
+###
+# create 2 vortices
+###
+
+@everywhere begin 
+	# double vortex on the same row
+	i1 = 25; j1 = 20
+	i2 = 25; j2 = 31
+	# double vortex on the same line
+	#i1 = 20; j1 = 25
+	#i2 = 31; j2 = 25
+	# double vortex in the diagonal
+	#i1 = 20; j1 = 20
+	#i2 = 31; j2 = 31
 end
 
-###
-# create vortex
-###
+T1 = create_vortex_on_sq_lattice(n,m,i1,j1)
+T2 = create_vortex_on_sq_lattice(n,m,i2,j2)
+#T2 = create_antivortex_on_sq_lattice(n,m,i2,j2)
+T = T1+T2
 
-# vortex or antivortex in the middle
-# choose middle square
-i = 25
-j = 25
-T = create_vortex_on_sq_lattice(n,m,i,j)
-#T = create_antivortex_on_sq_lattice(n,m,i,j)
+# central face cycle
+@everywhere begin 
+	ccycle1 = Int[]
+	ccycle2 = Int[]
+
+	push!(ccycle1, (i1-1)*n+j1)
+	push!(ccycle1, (i1-1)*n+j1+1)
+	push!(ccycle1, i1*n+j1+1)
+	push!(ccycle1, i1*n+j1)
+	
+	push!(ccycle2, (i2-1)*n+j2)
+	push!(ccycle2, (i2-1)*n+j2+1)
+	push!(ccycle2, i2*n+j2+1)
+	push!(ccycle2, i2*n+j2)
+end
 
 # set new angles
 change_T(s.g,T)
@@ -83,47 +98,60 @@ alphas = collect(low:step:high)
 # parallelization
 ###
 
-@everywhere has_moved = false
+@everywhere has_moved1 = false
+@everywhere has_moved2 = false
 
 # solver callback function
 @everywhere function callback_func(sp::SParams,n_iter::Int,error::Float64)
-	global has_moved
-	if !has_moved
-		# the vortex has moved?
-		if vorticity(sp.T,ccycle) == 0
-			has_moved = true
+	global has_moved1,has_moved2
+	if !has_moved1
+		# the vortex 1 has moved?
+		if vorticity(sp.T,ccycle1) == 0
+			has_moved1 = true
+		end
+	end
+	if !has_moved2
+		# the vortex 2 has moved?
+		if vorticity(sp.T,ccycle2) == 0
+			has_moved2 = true
 		end
 	end
 	return true
 end
 
 @everywhere function get_simulation_state(s::Simulator,P_ref::Array{Float64,1},alpha::Float64)
-	global has_moved = false
+	global has_moved1 = false
+	global has_moved2 = false
 	P = P_ref*alpha
 	change_P(s.g,P)
 	state = simulation(s,callback_func)
-	return alpha,state,has_moved
+	return alpha,state,has_moved1,has_moved2
 end
 
 # stats associated to a given random P
-type AStats
+type AStats2
 	# vortex stable & convergence
-	Vst::Dict{Float64,Float64}
-	# vortex inside & convergence
+	Vst1::Dict{Float64,Float64}
+	Vst2::Dict{Float64,Float64}
+	# vortices inside & convergence
 	Vin::Dict{Float64,Float64}
-	# vortex outside & convergence
-	Vout::Dict{Float64,Float64}
+	# 1 and 2 vortices outside & convergence
+	Vout1::Dict{Float64,Float64}
+	Vout2::Dict{Float64,Float64}
 	# no convergence
 	Div::Dict{Float64,Float64}
 end
 
 # initialize AStats
-stats = AStats(Dict{Float64,Float64}(),Dict{Float64,Float64}(),Dict{Float64,Float64}(),Dict{Float64,Float64}())
+astats2 = AStats2(Dict{Float64,Float64}(),Dict{Float64,Float64}(),Dict{Float64,Float64}(),Dict{Float64,Float64}(),Dict{Float64,Float64}(),Dict{Float64,Float64}())
+
 for alpha in alphas
-	stats.Vst[alpha] = 0.
-	stats.Vin[alpha] = 0.
-	stats.Vout[alpha] = 0.
-	stats.Div[alpha] = 0.
+	astats2.Vst1[alpha] = 0.
+	astats2.Vst1[alpha] = 0.
+	astats2.Vin[alpha] = 0.
+	astats2.Vout1[alpha] = 0.
+	astats2.Vout2[alpha] = 0.
+	astats2.Div[alpha] = 0.
 end
 
 # P -> (alpha -> symbol)
@@ -142,19 +170,26 @@ for i in 1:np
 	for r in results
 		alpha = r[1]
 		state = r[2]
-		has_moved = r[3]
+		has_moved1 = r[3]
+		has_moved2 = r[4]
 		if state.n_iter < max_iter 
-			if !has_moved
-				stats.Vst[alpha] += 1.
-				as[alpha] = :Vst
+			if !has_moved1 && !has_moved2
+				astats2.Vst1[alpha] += 1.
+				as[alpha] = :Vst1
+			elseif (!has_moved1 && has_moved2) || (has_moved1 && !has_moved2)
+				astats2.Vst2[alpha] += 1.
+				as[alpha] = :Vst2
 			else
 	 			v = vorticity(state.T,bcycle)
-				if v == 1
+				if v == 2
 					stats.Vin[alpha] += 1.
 					as[alpha] = :Vin
-				else
-					stats.Vout[alpha] += 1.
-					as[alpha] = :Vout
+				elseif v == 1
+					stats.Vout1[alpha] += 1.
+					as[alpha] = :Vout1
+				elseif v == 0
+					stats.Vout2[alpha] += 1.
+					as[alpha] = :Vout2
 				end
 			end
 		else
@@ -168,13 +203,15 @@ end
 
 # normalize
 for alpha in alphas
-	stats.Vst[alpha] /= np
-	stats.Vin[alpha] /= np
-	stats.Vout[alpha] /= np
-	stats.Div[alpha] /= np
+	astats2.Vst1[alpha] /= np
+	astats2.Vst2[alpha] /= np
+	astats2.Vin[alpha] /= np
+	astats2.Vout1[alpha] /= np
+	astats2.Vout2[alpha] /= np
+	astats2.Div[alpha] /= np
 end
 
-println(stats)
-serialize_to_file(stats, "AStats_$np-$low-$step-$high.jld")
-serialize_to_file(pstats, "PStats_$np-$low-$step-$high.jld")
+println(astats2)
+serialize_to_file(astats2, "AStats2_$np-$low-$step-$high.jld")
+serialize_to_file(pstats, "PStats2_$np-$low-$step-$high.jld")
 
